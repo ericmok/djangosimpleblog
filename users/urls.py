8f31ba28867b43b447a77671c90e15f3f1ad2fc1
Eric Mok <eric@mok.email>
2014-09-27 23:25:49 -0400

    Added singin to urls

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,7 @@ from users.views import RegisterView
 
 urlpatterns = patterns('',
     url(r'^register/?$', RegisterView.as_view(template_name='users/register.html'), name='users-register'),
+    url(r'^login/?$', 'django.contrib.auth.views.login', {
+            'template_name': 'users/signin.html'
+        }, name='users-signin')
 )
