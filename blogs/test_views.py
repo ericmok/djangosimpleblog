bff33338b4d93c68e40968130040804a172fdf4c
Eric Mok <eric@mok.email>
2014-09-27 23:09:08 -0400

    Added urls to connect to post create template

--- blogs/test_views.py
+++ blogs/test_views.py
@@ -1,11 +1,12 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 
+
 class PostViews(TestCase):
 
     def setUp(self):
         self.client = Client()
 
     def test_post_create(self):
-        response = self.client.get(reverse('post-create'))
+        response = self.client.get(reverse('posts-create'))
         self.assertEqual(response.status_code, 200)
--- urls.py
+++ urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls')),
+    url(r'^blogs/', include('blogs.urls'))
 )
