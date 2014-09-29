10dff7737dedc893086fa18f7ef87a614e423079
Eric Mok <eric@mok.email>
2014-09-29 15:16:00 -0400

    Changed url from /posts/create to /posts/new

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 from blogs.views import PostCreationView, PostDetailView
 
 urlpatterns = patterns('',
-    url(r'^create/?$', PostCreationView.as_view(), name='posts-create'),
+    url(r'^posts/new/?$', PostCreationView.as_view(), name='posts-create'),
     url(r'^posts/(?P<slug>.*?)/?$', PostDetailView.as_view(), name='posts-detail')
 )
