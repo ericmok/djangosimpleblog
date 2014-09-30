8da59834a96cffff7784cd634861cd2c97ebae66
Eric Mok <eric@mok.email>
2014-09-29 20:02:15 -0400

    Changed login/logout urls to signin/signout

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,10 @@ from users.views import RegisterView
 
 urlpatterns = patterns('',
     url(r'^register/?$', RegisterView.as_view(template_name='users/register.html'), name='users-register'),
-    url(r'^login/?$', 'django.contrib.auth.views.login', {
+    url(r'^signin/?$', 'django.contrib.auth.views.login', {
             'template_name': 'users/sign_in.html'
         }, name='users-signin'),
-    url(r'^logout/?$', 'django.contrib.auth.views.logout', {
+    url(r'^signout/?$', 'django.contrib.auth.views.logout', {
             'template_name': 'users/signed_out.html'
         }, name='users-signout')
 )
