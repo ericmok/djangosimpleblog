6faf4d437d0326da25ae327f99cb8983a7639a2c
Eric Mok <eric@mok.email>
2014-10-03 23:34:07 -0400

    Markdown filter registered

--- blogs/templatetags/present.py
+++ blogs/templatetags/present.py
@@ -7,9 +7,13 @@ import bleach
 register = template.Library()
 
 
-def present(value, arg):
-    tags = bleach.ALLOWED_TAGS.extend(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'cite', 'img'])
-    attributes = bleach.ALLOWED_ATTRIBUTES
-    attributes['img'] = ['src']
-    styles = bleach.ALLOWED_STYLES
-    return bleach.clean(markdown.markdown(value), tags=tags, attributes=attributes, styles=styles, strip=False, strip_comments=True)
+def inner_present(value):
+    bleach.ALLOWED_TAGS.extend(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'cite', 'img', 'p'])
+    bleach.ALLOWED_ATTRIBUTES['img'] = ['src']
+    markdown_text = markdown.markdown(value)
+    return bleach.clean(markdown_text, strip=False, strip_comments=True)
+
+
+@register.filter
+def present(value):
+    inner_present(value)
--- blogs/test_tags.py
+++ blogs/test_tags.py
@@ -15,6 +15,16 @@ class PresentTest(TestCase):
         markdown_test = present(markdown_test)
         self.assertIn('<h1>', markdown_test)
 
+    def test_markdown_takes_h1_and_img(self):
+        new_post = Post.objects.create_with_edition(title='test', author=self.new_user, text='#This is the post\n\n\nThis is a test\n\n\nAnother [here](cat.png)')
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Edition.objects.count(), 1)
+
+        markdown_test = Post.objects.get(id=new_post.id).editions.first().text
+        markdown_test = present(markdown_test)
+        self.assertIn('<h1>', markdown_test)
+        self.assertIn('src=\"cat.png\"', markdown_test)
+
     def test_markdown_is_safe(self):
         new_post = Post.objects.create_with_edition(title='test', author=self.new_user, text='<script></script>')
         self.assertEqual(Post.objects.count(), 1)
